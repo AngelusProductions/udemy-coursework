449b12b55ed8c6832c0f93a2369c8c24bdb60152
Corey Angelus <angelusproductions@gmail.com>
2019-05-14 12:16:31 -0400

    Add Test 2 of C# Fundamentals Course

---
+++ C#/FundamentalsOfCsharp/Test 1/FundamentalsOfCsharp-Test1/FundamentalsOfCsharp-Test1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace CSharpForBeginnersTest1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Valid();
+            MaxNum();
+            Orientation();
+            Speeding();
+            Thread.Sleep(2000);
+        }
+
+        public static void Valid()
+        {
+            Console.WriteLine("Enter #");
+            string input = Console.ReadLine();
+            int inputNum = Convert.ToInt32(input);
+            if (inputNum > 10)
+            {
+                Console.WriteLine("Invalid");
+            }
+            else
+            {
+                Console.WriteLine("Valid");
+            }
+        }
+
+        public static void MaxNum()
+        {
+            Console.WriteLine("Enter 1st #");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd #");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int[] nums = { num1, num2 };
+            Console.WriteLine($"Higher Number: {nums.Max()}");
+        }
+
+        public static void Orientation()
+        {
+            Console.WriteLine("Height?");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Width?");
+            int w = Convert.ToInt32(Console.ReadLine());
+            if (h > w)
+            {
+                Console.WriteLine("Portrait");
+            }
+            else
+            {
+                Console.WriteLine("Landscape");
+            }
+        }
+
+        public static void Speeding()
+        {
+            Console.WriteLine("Speed Limit?");
+            int limit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Speed?");
+            int speed = Convert.ToInt32(Console.ReadLine());
+            if (speed < limit)
+            {
+                Console.WriteLine("All gucci");
+            }
+            else
+            {
+                int difference = speed - limit;
+                double mod5 = difference / 5;
+                int demerits = Convert.ToInt32(Math.Round(mod5));
+                Console.WriteLine($"{demerits} demerits.");
+                if (demerits > 12)
+                {
+                    Console.WriteLine("License suspended.");
+                }
+            }
+
+
+        }
+    }
+}
