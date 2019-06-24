f35ec605ede4b99927961732546ec28b155e29d9
Corey Angelus <angelusproductions@gmail.com>
2019-06-23 23:09:14 -0400

    add stack designer class

---
+++ C#/Csharp Intermediate - Classes, Interfaces and OOP/StackDesigner/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace StackDesigner
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Initialize();
+            var input = string.Empty;
+            while (input != "done")
+            {
+                WriteLine("options:\n\n" +
+                	"push\npop\ndisplay\nclear\ndone\n");
+                switch (ReadLine())
+                {
+                    case "push":
+                        WriteLine("\npush what?");
+                        stack.Push(ReadLine());
+                        break;
+                    case "pop":
+                        WriteLine("\nhow many?");
+                        var number = int.Parse(ReadLine());
+                        WriteLine(stack.Pop(number));
+                        break;
+                    case "display":
+                        WriteLine(stack.Display());
+                        break;
+                    case "clear":
+                        WriteLine("\ncleared");
+                        stack.Clear();
+                        break;
+                    case "done":
+                        WriteLine("\nok bye");
+                        input = "done";
+                        break;
+                    default:
+                        WriteLine("\nwhat?");
+                        break;
+                }
+            }
+        }
+    }
+
+    public class Stack
+    {
+        private ArrayList _collection;
+
+        public void Push(object o)
+        {
+            if (o == null)
+                throw new InvalidOperationException();
+            _collection.Add(o);
+        }
+
+        public string Pop(int i)
+        {
+            if (_collection.Count < i || i == 0)
+                throw new InvalidOperationException();
+            var output = string.Empty;
+            for (int j = 0; j < i; j++)
+            {
+                output += $"{_collection[_collection.Count - 1]}\n";
+                _collection.RemoveAt(_collection.Count - 1);
+            }
+            return output;
+        }
+
+        public string Display ()
+        {
+            var output = string.Empty;
+            foreach (object o in _collection)
+               output += $"\n{o.ToString()}";
+            return output;
+        }
+
+        public void Initialize ()
+        {
+            var collection = new ArrayList
+               { "Corey", new int[] { 7, 8, 9 }, true,
+                  new List<DivideByZeroException>() };
+            _collection = collection;
+        }
+
+        public void Clear() { _collection.Clear(); }
+    }
+}
---
+++ C#/Csharp Intermediate - Classes, Interfaces and OOP/StackOverflowPost/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace StackOverflowPost
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            WriteLine("what's your problem?");
+            string title = ReadLine();
+            WriteLine("dang. that stinks. tell me more:");
+            string description = ReadLine();
+            WriteLine("jeez. can you show me a code snippet?");
+            string askSnippet = ReadLine();
+
+            var post = new Post(title, description);
+            if (new List<string> { "yes", "yea", "yeah", "y" }
+                .Contains(askSnippet.ToLower()))
+            {
+                WriteLine("ok. hit me:");
+                post.Code = ReadLine();
+            }
+
+            WriteLine("okeday, here's your post:");
+            WriteLine(post.Display());
+
+            Thread.Sleep(1000);
+            Write(".");
+            Thread.Sleep(500);
+            Write(".");
+            Thread.Sleep(500);
+            Write(".");
+            Thread.Sleep(1000);
+
+            var random = new Random();
+            WriteLine("\n\nwow look at your vote count now!");
+            for (int i = 0; i < 100; i++) post.Vote(random.Next(-1, 2));
+            WriteLine($"\nVote: {post.Votes}");
+        }
+
+        public class Post
+        {
+            private readonly DateTime _timestamp;
+            private readonly string _title, _description;
+
+            public Post(string title, string description)
+            {
+                _title = title;
+                _description = description;
+                _timestamp = DateTime.Now;
+            }
+
+            public int Votes { get; private set; } = 0;
+            public string Code { private get; set; } = string.Empty;
+            public void Vote(int direction) { Votes += direction; }
+
+            public string Display()
+            {
+                string body = $"\n{_title.ToUpper()}\n\n{_description}";
+                if (Code.Length > 0) body += $"\n[{Code}]";
+                return $"{body}\n\n{_timestamp.ToShortDateString()}\nVote: {Votes}\n";
+            }
+        }
+    }
+}
